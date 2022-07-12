86998ad330fd1cfcb876dcdc3a02200095d7cf16
Michael <mi.khai.thai@gmail.com>
2022-07-12 06:17:16 +0200

    remove unused code (tile id)

--- scrabble_python/items/tile.py
+++ scrabble_python/items/tile.py
@@ -1,14 +1,9 @@
-from uuid import uuid4
 from .helpers import create_distribution
 
 
-
 class Tile:
-    def __init__(self, letter: str, pos: tuple = None, tile_id: str = None, lang: str = 'fr'):
+    def __init__(self, letter: str, pos: tuple = None, lang: str = 'fr'):
         self.lang = lang
-        # self.__id = tile_id
-        # if tile_id is None:
-        #     self.__id = str(uuid4())
         self.letter = letter.upper()
         self.pos = pos
         if pos is not None:
@@ -17,9 +12,7 @@ class Tile:
             self.y = pos[1]
         self.value = self.__get_value()
 
-    # @property
-    # def id(self):
-    #     return self.__id
+
 
     def __get_value(self):
         distribution = create_distribution(self.lang, 'dict')
