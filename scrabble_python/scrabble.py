c93f996ed443b49b3f12c57d78824f8c84ff009b
Michael <mi.khai.thai@gmail.com>
2022-07-16 23:40:32 +0200

    implement end game

--- scrabble_python/scrabble.py
+++ scrabble_python/scrabble.py
@@ -24,14 +24,14 @@ class Scrabble:
         self.players_ids = [*self.players.keys()]
         self.nb_players = len(self.players)
 
-        self.BOARD_SIZE = scrabble_config.get(
-            'board_size', Scrabble.df_board_size)
-        self.RACK_SIZE = scrabble_config.get(
-            'rack_size', Scrabble.df_rack_size)
-        self.LANG = scrabble_config.get(
-            'lang', Scrabble.df_lang)
-        if self.LANG not in get_avail_langs():
-            raise UnavailableLanguage(unavail_lang=self.LANG)
+        self.config = {
+            'BOARD_SIZE': scrabble_config.get('board_size', Scrabble.df_board_size),
+            'RACK_SIZE': scrabble_config.get('rack_size', Scrabble.df_rack_size),
+            'LANG': scrabble_config.get('lang', Scrabble.df_lang)
+        }
+
+        if self.config['LANG'] not in get_avail_langs():
+            raise UnavailableLanguage(unavail_lang=self.config['LANG'])
 
         self.turn = scrabble_config.get('turn', 0)
         self.turn_rand = scrabble_config.get(
@@ -43,15 +43,18 @@ class Scrabble:
         self.board = board
         if purse is None or board is None:
             self.__initialize_game()
-        self.display_info()
 
     def __eq__(self, __o: object) -> bool:
         return isinstance(__o, Scrabble) and self.players == __o.players and self.turn == __o.turn and self.turn_id == __o.turn_id and self.history == __o.history and self.purse == __o.purse and self.board == __o.board
 
     def __initialize_game(self) -> None:
-        self.purse = Purse(lang=self.LANG)
-        self.board = Board(size=self.BOARD_SIZE)
-        for _ in range(self.RACK_SIZE):
+        self.purse = Purse(lang=self.config['LANG'])
+        print(self.purse)
+        print(len(self.purse))
+        self.board = Board(size=self.config['BOARD_SIZE'])
+        for pl_id in self.players:
+            self.players[pl_id].rack.clear()
+        for _ in range(self.config['RACK_SIZE']):
             for player_id in self.players:
                 rack = self.players[player_id].rack
                 rack.extend(self.purse.draw())
@@ -65,13 +68,21 @@ class Scrabble:
         self.history[self.turn] = new_words
         self.players[self.turn_id].score += scored_points
         self.board.add_tiles(move)
+
+        if not self.players[self.turn_id].rack:
+            self.end_game()
+
         self.turn += 1
         self.turn_id = self.__get_turn_id()
 
-    def display_info(self) -> None:
+    def __get_print_scores(self):
         scores = [self.players[player_id].score for player_id in self.players_ids]
         print('Scores:')
         print(*zip(self.players_ids, scores))
+        return scores
+
+    def display_info(self) -> None:
+        self.__get_print_scores()
         print(self.board)
         print(f'Player turn: {self.turn_id}')
         print(self.players[self.turn_id].rack)
@@ -91,10 +102,26 @@ class Scrabble:
         rack = self.players[self.turn_id].rack
         for letter in letters:
             rack.remove(Tile(letter))
-            rack.extend(self.purse.draw())
+            if len(self.purse) != 0:
+                rack.extend(self.purse.draw())
+
+    def end_game(self):
+
+        print(f'Game is finished by {self.turn_id}')
+        racks = [self.players[pl_id].rack for pl_id in self.players]
+        remaining_tiles = sum(racks, [])
+        print(remaining_tiles)
+        bonus_points = sum(tile.value for tile in remaining_tiles)
+        print(f'Finihser {self.turn_id} get {bonus_points} bonus_points by emptying his rack')
+        self.players[self.turn_id].score += bonus_points
+
+        final_scores = self.__get_print_scores()
+        winner_score = max(final_scores)
+        winner_idx = final_scores.index(winner_score)
+        winner = self.players_ids[winner_idx]
+        print(f'Winner is {winner} with a score of {winner_score}')
 
     # move: {(x, y): letter}, dict(pos:str)
-
     def submit(self, move) -> None:
         try:
             print('move submission')
--- tests/scrabble_tests/scrabble_test.py
+++ tests/scrabble_tests/scrabble_test.py
@@ -7,7 +7,7 @@ from scrabble_python.items.player import Player
 custom_scrabble_config = {
     'board_size': 11,
     'rack_size': 5,
-    'lang': 'en'
+    'lang': 'fr'
 }
 
 player1 = Player('player1')
@@ -16,22 +16,23 @@ player3 = Player('player3')
 player4 = Player('player4')
 
 
-def test_default_config_scrabble_init():
-    scrabble = Scrabble()
-    assert scrabble.BOARD_SIZE == Scrabble.df_board_size
-    assert scrabble.RACK_SIZE == Scrabble.df_rack_size
-    assert scrabble.LANG == Scrabble.df_lang
-    assert len(scrabble.players) == scrabble.nb_players == 2
-    assert all(len(scrabble.players[player_id].rack) == scrabble.RACK_SIZE for player_id in scrabble.players)
-    assert len(scrabble.purse) == 102 - \
-        (scrabble.RACK_SIZE*scrabble.nb_players)
-
-
-def test_custom_config_scrabble_init():
-    scrabble = Scrabble(**custom_scrabble_config)
-    assert scrabble.BOARD_SIZE == custom_scrabble_config['board_size']
-    assert scrabble.RACK_SIZE == custom_scrabble_config['rack_size']
-    assert scrabble.LANG == custom_scrabble_config['lang']
+def test_config_scrabble_init():
+    def_scrabble = Scrabble()
+    assert def_scrabble.config['BOARD_SIZE'] == Scrabble.df_board_size
+    assert def_scrabble.config['RACK_SIZE'] == Scrabble.df_rack_size
+    assert def_scrabble.config['LANG'] == Scrabble.df_lang
+    assert len(def_scrabble.players) == def_scrabble.nb_players == 2
+    assert all(len(def_scrabble.players[player_id].rack) == def_scrabble.config['RACK_SIZE'] for player_id in def_scrabble.players)
+    assert len(def_scrabble.purse) == 102 - \
+        (def_scrabble.config['RACK_SIZE']*def_scrabble.nb_players)
+    cust_scrabble = Scrabble(**custom_scrabble_config)
+    assert cust_scrabble.config['BOARD_SIZE'] == custom_scrabble_config['board_size']
+    assert cust_scrabble.config['RACK_SIZE'] == custom_scrabble_config['rack_size']
+    assert cust_scrabble.config['LANG'] == custom_scrabble_config['lang']
+    print([cust_scrabble.players[player_id].rack for player_id in cust_scrabble.players])
+    assert all(len(cust_scrabble.players[player_id].rack) == cust_scrabble.config['RACK_SIZE'] for player_id in cust_scrabble.players)
+    assert len(cust_scrabble.purse) == 102 - \
+        (cust_scrabble.config['RACK_SIZE']*cust_scrabble.nb_players)
 
 
 def test_unvalid_lang():
